b451aa7974ef62514991182a7af2b5ccd862bd6a
Charlton Tramel <ctramel14@gmail.com>
2024-08-21 11:12:05 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="card">
+      </div>
+    </>
+  )
+}
+
+export default App
